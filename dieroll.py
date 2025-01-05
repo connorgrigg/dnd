2f424bcd00ab9a24f08fb816ddaedef12863837b
connorgrigg <77861560+connorgrigg@users.noreply.github.com>
2025-01-05 14:58:41 -0600

    cleanup

--- dieroll.py
+++ dieroll.py
@@ -1,3 +1,6 @@
+#!/bin/python3
+
+
 import random
 import sys
 
@@ -38,19 +41,16 @@ if multiplier != "a":
 		plusout = int(plus[1])
 		is_plus = True
 		dice = plus[0]
-		print(dice)
 	else:
 		dice = multiplier[0]
 else:
-	print(parsed)
 	if parsed[1]:
 		plus = parsed[1].split("+")
 		plusout = int(plus[1])
 		is_plus = True
 		dice = plus[0]
-		print(dice)
 	else:
 		dice = parsed[1]
 roll(quantity,int(dice))
 
-# usage example: python3 dieroll.py 2D4X10+1
+# Usage example: python3 dieroll.py 2D4X10+5
