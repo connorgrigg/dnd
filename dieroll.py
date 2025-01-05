3d73c83a4a38a00b9964599fa31f92dbd2177529
connorgrigg <77861560+connorgrigg@users.noreply.github.com>
2025-01-05 15:12:15 -0600

    changes

--- dieroll.py
+++ dieroll.py
@@ -1,38 +1,43 @@
 #!/bin/python3
 
-
 import random
 import sys
 
 def roll(quantity, dice):
 	sum = 0
 	out = 0
-	for i in range(quantity):
-		out = random.randint(1,dice)
-		if is_plus:
-			out = out+plusout
-		print(out)
-		sum = sum+out
-	if quantity > 1:
-		print("Sum: ", sum)
-	if multiplier != "a":
-		print("Multiplied Sum: ",  sum*multiplierout)
+	try:
+		for i in range(quantity):
+			out = random.randint(1,dice)
+			if is_plus:
+				out = out+plusout
+			print(out)
+			sum = sum+out
+		if quantity > 1:
+			print("Sum: ", sum)
+		if multiplier != "a":
+			print("Multiplied Sum: ",  sum*multiplierout)
+	except Exception as e:
+		print("Invalid input - example input, 2d10+5")
 
 parsed = "a"
 multiplier = "a"
 plus = "1"
 is_plus = False
 input = sys.argv[1]
-if "D" in input:
-	parsed = input.split("D")
-elif "d" in input:
-	parsed = input.split("d")
-if "X" in input:
-	multiplier = parsed[1].split("X")
-	multiplierout = int(multiplier[1])
-elif "x" in input: 
-	multiplier = parsed[1].split("x")
-	multiplierout = int(multiplier[1])
+try:
+	if "D" in input:
+		parsed = input.split("D")
+	elif "d" in input:
+		parsed = input.split("d")
+	if "X" in input:
+		multiplier = parsed[1].split("X")
+		multiplierout = int(multiplier[1])
+	elif "x" in input:
+		multiplier = parsed[1].split("x")
+		multiplierout = int(multiplier[1])
+except Exception as e:
+	print("invalid input")
 
 quantity = int(parsed[0])
 if multiplier != "a":
@@ -53,4 +58,4 @@ else:
 		dice = parsed[1]
 roll(quantity,int(dice))
 
-# Usage example: python3 dieroll.py 2D4X10+5
+# usage example: python3 dieroll.py 2D4X10+1
