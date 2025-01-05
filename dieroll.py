8a231bb38fa9af3bd22cda8055241eba6324f910
connorgrigg <77861560+connorgrigg@users.noreply.github.com>
2025-01-05 14:25:26 -0600

    Add files via upload

---
+++ dieroll.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+
+import random
+import sys
+
+def roll(quantity, dice):
+	sum = 0
+	out = 0
+	for i in range(quantity):
+		out = random.randint(1,dice)
+		print(out)
+		sum = sum+out
+	if quantity > 1:
+		print("Sum: ", sum)
+	if multiplier != "a":
+		print("Multiplied Sum: ",  sum*multiplierout)
+
+parsed = "a"
+multiplier = "a"
+input = sys.argv[1]
+if "D" in input:
+	parsed = input.split("D")
+elif "d" in input:
+	parsed = input.split("d")
+if "X" in input:
+	multiplier = parsed[1].split("X")
+	multiplierout = int(multiplier[1])
+elif "x" in input: 
+	multiplier = parsed[1].split("x")
+	multiplierout = int(multiplier[1])
+quantity = int(parsed[0])
+if multiplier != "a":
+	dice = multiplier[0]
+else:
+	dice = parsed[1] 
+roll(quantity,int(dice))
+
+# 2D4X10
